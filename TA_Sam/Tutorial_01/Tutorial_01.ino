40ef5848aa28070bd4f82986c514fe130e9b58bd
sambabug <sambabug@gmail.com>
2019-10-05 01:26:49 +0530

    Update Tutorial_01.ino

--- TA_Sam/Tutorial_01/Tutorial_01.ino
+++ TA_Sam/Tutorial_01/Tutorial_01.ino
@@ -35,14 +35,17 @@
 //Energy meter defines
 #define EM_POLLING_INTERVAL            10000
 #define EM_ADDRESS                     0x01
-#define EM_REG_VOLT                    142
-#define EM_REG_CURR                    144
-#define EM_REG_APWR                    146
+#define EM_REG_ADDR                    142
+#define EM_REG_LEN                     16
 
 void setup_i2c();
 void setup_modbus();
 void sensor_temp_rh();
 void energy_meter();
+
+float* sht21_buffer = new float[2];
+float* em_buffer = new float[EM_REG_LEN/2];
+    
 
 esp32ModbusRTU modbus(&Serial1, UART_RTS);
 
@@ -50,18 +53,20 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(CONSOLE_BAUD_RATE);
 
-  Serial.println("Starting I2C");
+  Serial.printf("Starting I2C...");
   setup_i2c();
   if (!Wire1.begin(I2C_SDA, I2C_SCL)) {
-    Serial.println("Failed to start I2C!");
+    Serial.printf("Failed to start I2C!\n");
     while (1);
   }
+  Serial.printf("Success!");
+  
 
-  Serial.println("Start Modbus RTU");
+  Serial.printf("Start Modbus RTU...");
   // start the Modbus RTUs
   Serial1.begin(UART_BAUD_RATE, UART_CONFIG, UART_RX, UART_TX);
   setup_modbus();
-
+  Serial.printf("Done!");
 }
 
 void loop() {
@@ -79,17 +84,26 @@ void setup_i2c() {
 void setup_modbus() {
   //modbus setup
   modbus.onData([](uint8_t serverAddress, esp32Modbus::FunctionCode fc, uint8_t* data, size_t length) {
-    Serial.printf("id 0x%02x fc 0x%02x len %u: 0x", serverAddress, fc, length);
+    //Serial.printf("id 0x%02x fc 0x%02x len %u: 0x", serverAddress, fc, length);
+    
+    uint8_t* buffer = new uint8_t[length];
     for (size_t i = 0; i < length; ++i) {
-      Serial.printf("%02x", data[i]);
+      (i%2 == 0) ? buffer[i] = data[i+1] : buffer[i] = data[i-1];
+      //Serial.printf(" d:%02x", data[i]);
+      //Serial.printf(" b:%02x", buffer[i]);
     }
-    //std::reverse(data, data + 4);  // fix endianness
-    uint8_t data2[4];
-    data2[0] = data[1];
-    data2[1] = data[0];
-    data2[2] = data[3];
-    data2[3] = data[2];
-    Serial.printf("\nval: %.2f\n", *(float*)data2);//*reinterpret_cast<float*>(data2));
+    //Serial.printf(" DONE\n");
+
+    em_buffer = (float*) buffer;
+
+    Serial.printf("em_buffer[0]: %.2f", em_buffer[0]);
+    Serial.printf(", em_buffer[1]: %.2f", em_buffer[1]);
+    Serial.printf(", em_buffer[2]: %.2f", em_buffer[2]);
+    Serial.printf(", em_buffer[3]: %.2f\n", em_buffer[3]);
+    Serial.printf("em_buffer[4]: %.2f", em_buffer[4]);
+    Serial.printf(", em_buffer[5]: %.2f", em_buffer[5]);
+    Serial.printf(", em_buffer[6]: %.2f", em_buffer[6]);
+    Serial.printf(", em_buffer[7]: %.2f\n", em_buffer[7]);
   });
   modbus.onError([](esp32Modbus::Error error) {
     Serial.printf("error: 0x%02x\n", static_cast<uint8_t>(error));
@@ -101,9 +115,9 @@ void sensor_temp_rh() {
   static uint32_t lastMillisSH21 = 0;
   if (millis() - lastMillisSH21 > SHT21_POLLING_INTERVAL) {
     lastMillisSH21 = millis();
-    float rel_humidity = SHT2x.GetHumidity();
-    float temperature = SHT2x.GetTemperature();
-    Serial.printf("Humidity(%RH): %.2f, Temperature(C): %.2f\n", rel_humidity, temperature);
+    sht21_buffer[0] = SHT2x.GetHumidity();
+    sht21_buffer[1] = SHT2x.GetTemperature();
+    Serial.printf("Humidity(%RH): %.2f, Temperature(C): %.2f\n", sht21_buffer[0], sht21_buffer[1]);
   }
 }
 
@@ -111,11 +125,7 @@ void energy_meter() {
   static uint32_t lastMillisEM = 0;
   if (millis() - lastMillisEM > EM_POLLING_INTERVAL) {
     lastMillisEM = millis();
-    Serial.print("sending Modbus request...\n");
-    modbus.readHoldingRegisters(EM_ADDRESS, EM_REG_VOLT, 2);
-    //digitalWrite(UART_RTS, HIGH);
-    //Serial1.print("Hello");
-    //delay(1);
-    //digitalWrite(UART_RTS, LOW);
+    //Serial.print("sending Modbus request...\n");
+    modbus.readHoldingRegisters(EM_ADDRESS, EM_REG_ADDR, EM_REG_LEN);
   }
 }
